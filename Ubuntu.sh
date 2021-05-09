4cbdd99625884731d2ee2511ba806c8ee4026487
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-09 16:23:21 +0800

    Create Ubuntu.sh

---
+++ Ubuntu.sh
@@ -0,0 +1,13 @@
+pkg update
+pkg upgrade 
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install git 
+pkg install wget 
+pkg install nano 
+pkg install proot 
+pkg install cmake 
+pkg install make 
+sleep 2
+clear
